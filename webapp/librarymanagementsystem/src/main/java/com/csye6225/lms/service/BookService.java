d6023ac3e7778798a65af5e54e1315319c306f8d
yogitaneu <patil.yo@husky.neu.edu>
2019-06-07 00:44:04 -0400

    Adding Books API methods Get/Put/Delete

---
+++ webapp/librarymanagementsystem/src/main/java/com/csye6225/lms/service/BookService.java
@@ -0,0 +1,33 @@
+package com.csye6225.lms.service;
+
+import com.csye6225.lms.dao.BookRepository;
+import com.csye6225.lms.pojo.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    public Book save(Book book){
+        return bookRepository.save(book);
+    }
+
+    public Optional<Book> findById(UUID id){
+        return bookRepository.findById(id);
+    }
+
+    public List<Book> findAll(){
+        return bookRepository.findAll();
+    }
+
+    public void deleteById(UUID id){
+        bookRepository.deleteById(id);
+    }
+}
