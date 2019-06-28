7d2b189985e595e2202bc5a9d93ae38a6a1750bc
Jagman <kaur.j@husky.neu.edu>
2019-06-28 00:41:32 -0400

    application delete script

---
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-application-stack.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#shell script to create AWS application infrastructures
+
+ help_me()
+{
+    echo "Usage:-"
+    echo "$0 <stack name> "
+    exit
+}
+STACK_NAME=$1
+
+if [ -z "$1" ]
+  then
+    echo "No STACK_NAME argument supplied"
+    help_me
+fi
+
+echo "Deleting Application stack..."
+
+aws cloudformation delete-stack --stack-name $STACK_NAME
+
+if [ $? -eq 0 ]; then
+  echo "Delete in progress"
+  aws cloudformation wait stack-delete-complete --stack-name $STACK_NAME
+  echo "Stack deleted successfully"
+else
+  echo "Failure while deleting stack"
+fi
